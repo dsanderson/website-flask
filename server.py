3ea7e1f4aab079cbc13749d5a7f16e07229b834d
dsanderson <the.real.david.anderson@gmail.com>
2017-01-09 23:03:23 +0800

    adding basic login functionality

--- server.py
+++ server.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request
+import secrets
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hello World!"
+	if 'username' in session:
+		return "Hello {}!".format(session['username'])
+	else:
+		return "Hello Guest!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+		name = request.form['username']
+		passwd = request.form['password']
+		if name in secrets.users.keys():
+			if secrets.users[name] == passwd:
+        		session['username'] = request.form['username']
+        		return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+			<p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/aircon/contoller',methods=["GET"])
+def aircon_controller():
+	pass
+
+@app.route('/aircon/contoller',methods=["POST"])
+def aircon_setter():
+	pass
+
+@app.route('/aircon/command'):
+def aircon_command():
+	pass
+
+# set the secret key.  keep this really secret:
+app.secret_key = secrets.key
 
 if __name__ == '__main__':
 	app.run()
-
