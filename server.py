8d65d53cbeefd1651a84482a947015f709b3a247
dsanderson <the.real.david.anderson@gmail.com>
2017-01-09 23:16:32 +0800

    adding debugging

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ def login():
 			if secrets.users[name] == passwd:
 				session['username'] = request.form['username']
 				return redirect(url_for('index'))
+		return "{}, {}".format(name, passwd)
 	return '''
 	<form method="post">
 	<p><input type=text name=username>
