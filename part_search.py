68e244b7775ff25d5a869238e6265a24e8dcf63b
dsanderson <the.real.david.anderson@gmail.com>
2017-09-03 01:23:58 +0800

    still removing unit_text bits

--- part_search.py
+++ part_search.py
@@ -83,7 +83,9 @@ def fetch_document_data(doc_ids, query):
     for q in query:
         if q[1]!='':
             keywords = [t.strip().lower() for t in q[0].split(',')]
-            db_query = session.query(Scraped_Site.id, sqla.func.array_agg(Unit_Num.value)).join(Unit_Num).join(Unit_Text).filter(Scraped_Site.id==Unit_Num.source).filter(Unit_Text.unit==Unit_Num.id).filter(Unit_Text.document==Scraped_Site.id).filter(Unit_Num.unit_type==q[1]).filter(sqla.or_(*[sqla.and_(Unit_Text.unit==Unit_Num.id, Unit_Text.text.ilike('%{}%'.format(t))) for t in keywords])).filter(Scraped_Site.id.in_(ids)).group_by(Scraped_Site.id)
+            #db_query = session.query(Scraped_Site.id, sqla.func.array_agg(Unit_Num.value)).join(Unit_Num).join(Unit_Text).filter(Scraped_Site.id==Unit_Num.source).filter(Unit_Text.unit==Unit_Num.id).filter(Unit_Text.document==Scraped_Site.id).filter(Unit_Num.unit_type==q[1]).filter(sqla.or_(*[sqla.and_(Unit_Text.unit==Unit_Num.id, Unit_Text.text.ilike('%{}%'.format(t))) for t in keywords])).filter(Scraped_Site.id.in_(ids)).group_by(Scraped_Site.id)
+            #TODO fix unit_text part
+            db_query = session.query(Scraped_Site.id, sqla.func.array_agg(Unit_Num.value)).join(Unit_Num).filter(Scraped_Site.id==Unit_Num.source).filter(Unit_Num.unit_type==q[1]).filter(Scraped_Site.id.in_(ids)).group_by(Scraped_Site.id)
             vals = db_query.order_by(Scraped_Site.id.desc()).all()
             vals = [v[1] for v in vals]
             data.append(tuple(vals))
